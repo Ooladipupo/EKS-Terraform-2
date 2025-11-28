e691287dc1db2970ffc7abee57c21552856a639c
Oladipupo Olufowobi <olufowobi2234@gmail.com>
2025-11-28 22:08:32 +0000

    Refactor comments in IAMRole&Policy.tf for clarity
    Corrected comments for clarity and consistency in IAM role definitions.

--- IAMRole&Policy.tf
+++ IAMRole&Policy.tf
@@ -1,11 +1,11 @@
 
-#we want to create a role to be used by our kUBERNETES developers and Adminstrators. 
-#Please note that this role is different from the cluster role to be used by our kubernetes cluster and the node group role
+# We want to create a role to be used by our Kubernetes developers and administrators. 
+#Please note that this role is different from the cluster role to be used by our Kubernetes cluster and the node group role
 
 
 
 
-#the below is an AWS IAM specific role. We are yet to do anything with EKS RBAC at this point, we would give it also an inline policy
+# The below is an AWS IAM-specific role. We are yet to do anything with EKS RBAC at this point, we would also give it an inline policy
 resource "aws_iam_role" "external-developer" {
   name = "external-developer"
 
@@ -24,8 +24,8 @@ resource "aws_iam_role" "external-developer" {
   })
 }
 
-#note that the use of terraform inline_policy has been deprecated. hence, we need to create our policy or use any of the customer policies on AWS
-#then we use the aws_iam_role_policy_attachment to attach the policy to the role. as you would see below. 
+#Note that the use of Terraform inline_policy has been deprecated. Hence, we need to create our policy or use any of the customer policies on AWS
+#then we use the aws_iam_role_policy_attachment to attach the policy to the role, as you would see below. 
 resource "aws_iam_role_policy_attachment" "devopsshack_cluster_role_policy_for_developer" {
   role       = aws_iam_role.external-developer.name
   policy_arn = "arn:aws:iam::457082365292:policy/DescribeCluster"
@@ -33,7 +33,7 @@ resource "aws_iam_role_policy_attachment" "devopsshack_cluster_role_policy_for_d
 
 
 
-#lets create a similar role for the kubernetes administrator and attached the same 
+# Let's create a similar role for the Kubernetes administrator and attach the same 
 resource "aws_iam_role" "external-admin" {
   name = "external-admin"
 
@@ -52,16 +52,16 @@ resource "aws_iam_role" "external-admin" {
   })
 }
 
-#note that the use of terraform inline_policy has been deprecated. hence, we need to create our policy or use any of the customer policies on AWS
-#then we use the aws_iam_role_policy_attachment to attach the policy to the role. as you would see below. 
+#note that the use of Terraform inline_policy has been deprecated. Hence, we need to create our policy or use any of the customer policies on AWS
+#then we use the aws_iam_role_policy_attachment to attach the policy to the role, as you would see below. 
 resource "aws_iam_role_policy_attachment" "devopsshack_cluster_role_policy_for_admin" {
   role       = aws_iam_role.external-admin.name
   policy_arn = "arn:aws:iam::457082365292:policy/DescribeCluster"
 }
 
 
-#Next is to map the above IAM roles to our kubernetes. prior to know, the mapping is done using aws_auth configmap resources, 
-#now deprecated and replaced with eks api resouce called aws_wks_access_entry
+#Next is to map the above IAM roles to our Kubernetes. Prior to know, the mapping is done using aws_auth configmap resources. 
+#now deprecated and replaced with the eks api resource called aws_wks_access_entry
 
 
 resource "aws_eks_access_entry" "developer" {
@@ -79,6 +79,6 @@ resource "aws_eks_access_entry" "admin" {
 resource "aws_eks_access_entry" "github_oidc" {
   cluster_name = aws_eks_cluster.devopsshack.name
   principal_arn = "arn:aws:iam::457082365292:role/Github-action"
-  kubernetes_groups = ["system:masters"]
+  user_name         = "Github-action"
   type = "STANDARD"
 }
